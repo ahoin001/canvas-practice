2befdd74337ac1ce0babe7d0f6ecc3c61d46de85
Alexander Hoinville <ahoin001@gmail.com>
2019-07-06 15:03:29 -0400

    explanation added

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 console.log("I'm connected!");
 
-let myDiv = document.querySelector('#thing');
-myDiv.innerHTML = "<h1> Blah </h1> ";
-myDiv.innerHTML += "<h2> kazam </h2>";
+// let myDiv = document.querySelector('#thing');
+// myDiv.innerHTML = "<h1> Blah </h1> ";
+// myDiv.innerHTML += "<h2> kazam </h2>";
 
 function moveRect(x, y) {
 
@@ -15,6 +15,7 @@ function moveRect(x, y) {
     const ctx = theCanvas.getContext("2d");
 
     // clear whole canvas to simulate animation
+    // makes clear rectangel the size of canvas to make canvas look clear
     // 0 => x
     // 0 => y
     // 300 => width (comes from index.html where we defined it)
@@ -23,13 +24,16 @@ function moveRect(x, y) {
 
 
 
-    // colors rectangle in the given color
+    // colors rectangle in the given color (before its created)
     ctx.fillStyle = "red";
 
     // given method that creates rectangle - ctx.fillRect(x, y, width, height)
     ctx.fillRect(x, y, 50, 50);
-
+    // ctx.fillStyle = "red";
+    
+    // move rect on x axis by 3
     x += 3;
+    // whenever it is divisble on x axis by 10, move down 5 
     if (x % 10 === 0) {
         y += 5;
     }
@@ -37,4 +41,10 @@ function moveRect(x, y) {
     // calls itself every 30ms => RECURSIVE FUNCTION
     setTimeout(`moveRect(${x}, ${y})`, 30);
 
+
+    // summary: 
+    // makes canvas appear clear with clear rect
+    // create a new filled red square
+    // move rect 3px on x
+    // function calls itself then quickly clearrects the canvas and then places red square in new position it looks like a smooth animation
 }
